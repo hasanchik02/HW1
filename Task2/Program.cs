07f27e82602a90e16bf97eeef2143a2e8a628e21
Slava <Slava9162004@yandex.ru>
2022-12-18 14:00:45 +0300

    Большее число и меньшее

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает 
+//два числа и выдает, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите два числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("Большое число");
+    Console.WriteLine(number1);
+    Console.WriteLine("Меньшее число");
+    Console.WriteLine(number2);
+}
+if (number1 < number2)
+{
+    Console.WriteLine("Большое число");
+    Console.WriteLine(number2);
+    Console.WriteLine("Меньшее число");
+    Console.WriteLine(number1); 
+}
+if (number1 == number2)
+{
+    Console.WriteLine("Числа одинаковые");
+    Console.WriteLine(number1);
+}

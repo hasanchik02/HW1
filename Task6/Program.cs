19a235f16454c9fc4df2fb259721518a82e3f471
Slava <Slava9162004@yandex.ru>
2022-12-18 14:36:19 +0300

    Проверка на четность

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int remains = number %2;
+
+if (remains > 0)
+{
+    Console.WriteLine($"Число {number} не является четным числом");
+}
+else
+{
+    Console.WriteLine($"Число {number} является четным числом");
+}
